6a1f322a76d37ff76c4d2286ecb1cb1321bece5e
up250001 <up250001@alumnos.upa.edu.mx>
2025-03-31 14:26:47 -0600

    Add files via upload

---
+++ 08_ Dayexercises.py
@@ -0,0 +1,40 @@
+dog = {}
+
+dog = {'name':'Tito',
+       'color':'white and brown',
+       'breed':'Beagle',
+       'legs':4,
+       'age':8}
+
+student = {
+    'first_name':'Guillermo',
+    'last_name':'Alvarez',
+    'gender':'Male',
+    'age':19,
+    'marital_status':'single',
+    'skills':['Python','SQL','HTML','CSS','JS'],
+    'country':'Mexico',
+    'city':'Aguascalientes',
+    'addres':{
+        'street':'vallarta Street',
+        'zipcode':'NFX 6NK',}}
+
+print(f'The lenght of the student dictionary is {len(student)}')
+print(f'The data type of student skills is {type(student["skills"])}')
+
+student['skills'].append('PHP')
+print(student['skills'])
+
+keyList = list(student.keys())
+print(keyList)
+
+valList = list(student.values())
+print(valList)
+
+studentTuple = student.items()
+print(studentTuple)
+
+student.pop('gender')
+print(student)
+
+del dog
